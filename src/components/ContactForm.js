d818586555672794b70aa5a1223f6f1b8ee2680f
Hugo Hashi <hugo_hashimoto@hotmail.com>
2023-10-10 17:53:01 +0100

    Rewrote the readme file and adjusted the contact form component

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,49 +1,46 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import "./contactform.css"
 
-function ContactForm() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
+const ContactForm = () => {
+    const [formState, setFormState] = useState({});
 
-  function sendEmail(e){
-    e.preventDefault();
-    alert("Mensagem Enviada")
-  }
+    const changeHandler = (event) => {
+        setFormState({...formState, [event.target.name]: event.target.value});
+    }
 
-  return (
-    <div className="contact-form">
-      <h1 className="message-title">Envie Sua Mensagem</h1>
+    const sendMessage = (event) => {
+        event.preventDefault();
 
-      <form className="form" onSubmit={() => {}}>
-        <input 
-          className="input"
-          type="text"
-          placeholder="Digite seu nome"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-        
-        <input 
-          className="input"
-          type="text"
-          placeholder="Digite seu email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
+        alert("Mensagem Enviada! Assim que possível entraremos em contacto.")
 
-        <textarea 
-          className="textarea"
-          placeholder="Digite sua mensagem..."
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-        />
+        window.Email.send({
+            Host : "smtp.elasticemail.com",
+            Username : "",
+            Password : "",
+            To : "",
+            From : "",
+            Subject : "Nova requisição de contacto",
+            Body : "Name: " + document.getElementById("name").value + 
+            "<br> Telemóvel: " + document.getElementById("phone").value +
+            "<br> E-mail: " + document.getElementById("email").value +
+            "<br> Mensagem: " + document.getElementById("message").value 
+        }).then(
+            message => alert(message)
+        );
+    }
 
-        <input className="send_button" type="submit" value="Enviar" />
-      </form>
-
-    </div>
-  );
-}
+    return(
+        <div className="contact_form">
+            <form method='post' onSubmit={sendMessage}>
+                <h2>Envie Sua Mensagem</h2>
+                <input type="text" id="name" placeholder="Nome" onChange={changeHandler}/>
+                <input type="phone" id="phone" placeholder="Telemóvel" onChange={changeHandler}/>
+                <input type="email" id="email" placeholder="E-mail" onChange={changeHandler}/>
+                <textarea id="message" rows={10} placeholder="Mensagem" onChange={changeHandler}/>
+                <button type="submit">Enviar Mensagem</button>
+            </form>
+        </div>
+    );
+};
 
 export default ContactForm;
